ba41f5884b5c63d4ce22c21d167f108019a65124
Olga Mirete <olgamirete@gmail.com>
2022-07-19 13:46:27 +0200

    generalized code to make it easier to use externally

--- binance_get_best_rates.py
+++ binance_get_best_rates.py
@@ -1,13 +1,13 @@
 import trio
 import json
 import os
-from datetime import datetime
-from httpx import AsyncClient
-import trio
 import pandas as pd
-from binance_helpers import get_fiat_cost, get_p2p_offers, relevant_coins
 import matplotlib.pyplot as plt
 import seaborn as sns
+from datetime import datetime
+from httpx import AsyncClient
+from binance_helpers import get_fiat_cost, get_p2p_offers, relevant_coins
+from functools import partial
 
 
 def plot_rates(df):
@@ -17,7 +17,7 @@ def plot_rates(df):
     plt.show()
 
 
-async def main(offline_test: bool = False):
+async def main(offline_test: bool = False, print_table: bool = False, show_plot: bool = False) -> str:
     async with AsyncClient() as client:
         fiat_cost = await get_fiat_cost(client, "EUR", offline_test=offline_test)
 
@@ -26,6 +26,7 @@ async def main(offline_test: bool = False):
             p2p_offers[coin] = await get_p2p_offers(client, coin, 'ARS', "40000", offline_test=offline_test)
 
         rates = {}
+        msg = ''
         for coin in relevant_coins:
             price_start_currency = fiat_cost[coin]["quotation"]
             msg_line = f"{coin}:"
@@ -35,10 +36,14 @@ async def main(offline_test: bool = False):
                 rate = float(price_end_currency) / float(price_start_currency)
                 rates[coin].append(round(rate, 2))
                 msg_line += f"\t{rates[coin][i]}"
-            print(msg_line)
+            msg += f"{msg_line}\n"
 
-        df = pd.DataFrame(rates)
-        plot_rates(df)
+        if print_table == True:
+            print(msg)
+
+        if show_plot == True:
+            df = pd.DataFrame(rates)
+            plot_rates(df)
 
         if offline_test == False:
             timestamp = datetime.now().isoformat()[:19].replace(":", "-")
@@ -50,4 +55,23 @@ async def main(offline_test: bool = False):
             with open(os.path.join(folder, 'p2p.txt'), 'w', encoding='utf-8') as f:
                 f.write(json.dumps(p2p_offers))
 
-trio.run(main, (False))
+        return msg
+
+
+def getRatesTable() -> str:
+    func = partial(main,
+                   offline_test=False,
+                   print_table=False,
+                   show_plot=False
+                   )
+    rates_table = trio.run(func)
+    return rates_table
+
+
+if __name__ == "__main__":
+    func = partial(main,
+                   offline_test=False,
+                   print_table=True,
+                   show_plot=True
+                   )
+    trio.run(func)
